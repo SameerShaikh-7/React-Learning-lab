58f9f2b78fdcaa190d0864b282105664adafdef0
SameerShaikh-7 <shaikh.sameer.76220@gmail.com>
2026-03-25 16:36:01 +0530

    React

---
+++ MY-FIRST-REACT-APP/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return <>
+    <div>
+        <Header />
+      <p>My first React App</p>
+    </div>
+  </>
+
+    ;
+}
+
+export default App; 
---
+++ MY-TYPSCRIPT-APP/MY-TPYESCRIPT-APP/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xlxlxl underline text-amber-300">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
